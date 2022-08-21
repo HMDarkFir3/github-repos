9152e309d8e3e79bc9d870d770ebb7c90e6414b3
Henrique Marques <henrique_marques.dev@outlook.com>
2022-08-20 23:07:24 -0300

    feat(*): add path

--- App.tsx
+++ App.tsx
@@ -2,10 +2,12 @@ import { StatusBar } from "expo-status-bar";
 import React, { FC } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+import { Home } from "@screens/Home";
+
 export const App: FC = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Home />
       <StatusBar style="auto" />
     </View>
   );
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,25 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          extensions: [".ts", ".tsx", ".jsx", ".js", ".json"],
+          alias: {
+            "@assets": "./src/assets",
+            "@components": "./src/components",
+            "@contexts": "./src/contexts",
+            "@hooks": "./src/hooks",
+            "@routes": "./src/routes",
+            "@screens": "./src/screens",
+            "@themes": "./src/themes",
+            "@utils": "./src/utils",
+          },
+        },
+      ],
+    ],
   };
 };
